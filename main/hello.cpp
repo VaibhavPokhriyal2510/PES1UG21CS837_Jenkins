8fb0ca710f1e689a118aa11168f5e60e259bf83f
VaibhavPokhriyal2510 <79743672+VaibhavPokhriyal2510@users.noreply.github.com>
2023-02-19 15:48:01 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run with SRN-PES1UG21CS837\n";
+  cout << "I have successfully built and run with SRN-PES1UG21CS837 from PES\n";
   return 0;
 }
